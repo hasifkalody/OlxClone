01792f3912a7afdd521103419f9ef5b4a25fa881
HASIF <asifalikalody@gmail.com>
2022-12-22 11:33:59 +0530

    dropdown issue exist at MyAdsCard

--- src/Components/MyAdsCard/MyAdsCard.js
+++ src/Components/MyAdsCard/MyAdsCard.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Heart from '../../assets/Heart'
 import HeartActive from '../../assets/HeartActive'
 import Eye from '../../assets/MyAdsPage/Eye'
 import './MyAdsCard.css'
 function MyAdsCard({Items}) {
     const [showDrpDwn, setshowDrpDwn] = useState("empty")
-    const fn=()=>{document.getElementById(showDrpDwn).style.display="none";console.log("body")}
+    const fn=()=>{console.log("at fn");console.log(showDrpDwn);
+        if(showDrpDwn!="empty"){document.getElementById(showDrpDwn).style.display="none";console.log("body")}}
+    
+        useEffect(() => {
+            document.addEventListener("click", fn);
+            // return () => {
+            //     document.body.removeEventListener('click',fn);
+            // };
+        }, [])
   return (
-   <div className='AdCrd_body' onClick={fn }>
+   <div className='AdCrd_body'>
      {Items.map((x,ind)=>< div className='AdCrd_Container' >
         <div className='AdCrd_left'>
             <div><span>From:<b>Dec 20, 22</b></span></div>
@@ -34,9 +42,11 @@ function MyAdsCard({Items}) {
                     This ad is currently live
                 </div>
             </div>
-            <div className='AdCrd_5th' onClick={()=>{document.getElementById(ind).style.display="block"
+            <div className='AdCrd_5th' onClick={()=>{document.getElementById(ind).style.display="block";console.log("atbutton")
+                   setshowDrpDwn(ind)
                    if (ind==showDrpDwn){setTimeout(() => {document.getElementById(ind).style.display="block"}, 1)}
-                setshowDrpDwn(ind)}}>
+                   }}>
+                  
                <div id={ind}  className='AdCrd_5thdrpdwn ' >
                     <div><button id='AdCrd_5thdrpdwnBtn1'>Edit</button></div>
                     <div><button id='AdCrd_5thdrpdwnBtn2'>Remove</button></div>
